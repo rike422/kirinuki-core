ceb818fdd1bcc2130392d7a73cb6817c0a721402
Akira Takahash <rike422@gmail.com>
2018-08-06 11:42:09 +0900

    Change linter to use prettier insted of eslint

--- src/kirinuki.js
+++ src/kirinuki.js
@@ -1,10 +1,10 @@
-import $ from 'cheerio';
-import pluralize from 'pluralize';
-import unfold from './unfold';
-import getNodeValue from './get-node-value';
+import $ from "cheerio";
+import pluralize from "pluralize";
+import unfold from "./unfold";
+import getNodeValue from "./get-node-value";
 
 function isString(str) {
-  return (typeof str === 'string' || str instanceof String);
+  return typeof str === "string" || str instanceof String;
 }
 
 function isPlural(str) {
@@ -20,45 +20,49 @@ function convertObject(object, _$) {
 
   const rootElement = root != null ? $.load(_$(root).html()) : _$;
 
-  const scrap = function (selector, attr) {
-    const node = rootElement(selector).get(0)
+  const scrap = function(selector, attr) {
+    const node = rootElement(selector).get(0);
     if (node === undefined || node === null) {
       return void 0;
     }
     if (attr === undefined) {
       return getNodeValue(node);
     } else {
-      return node.attribs[attr]
+      return node.attribs[attr];
     }
   };
 
-  const scrapElements = function (selector, attr) {
+  const scrapElements = function(selector, attr) {
     if (attr === undefined) {
-      return rootElement(selector).toArray().map((tag, i) => {
-        return getNodeValue(tag);
-      });
+      return rootElement(selector)
+        .toArray()
+        .map((tag, i) => {
+          return getNodeValue(tag);
+        });
     } else {
-      return rootElement(selector).toArray().map((tag, i) => {
-        return tag.attribs[attr]
-      });
+      return rootElement(selector)
+        .toArray()
+        .map((tag, i) => {
+          return tag.attribs[attr];
+        });
     }
   };
 
   const convertedObject = Object.keys(schema).reduce((converted, key) => {
     const selector = schema[key];
-    const scrapper = (isUnfoldMode || isPlural(key)) ? scrapElements : scrap;
+    const scrapper = isUnfoldMode || isPlural(key) ? scrapElements : scrap;
 
     if (isString(selector)) {
       converted[key] = scrapper(selector);
     } else if (Array.isArray(selector)) {
       converted[key] = scrapper.apply(null, selector);
-    } else if (selector != null && typeof selector === 'object') {
+    } else if (selector != null && typeof selector === "object") {
       const data = convertObject(selector, rootElement);
-      converted[key] = (Array.isArray(data) && !isPlural(key)) ? data[0] : data;
+      converted[key] = Array.isArray(data) && !isPlural(key) ? data[0] : data;
     }
     return converted;
   }, {});
-  return (isUnfoldMode) ? unfold(convertedObject) : convertedObject;
+  return isUnfoldMode ? unfold(convertedObject) : convertedObject;
 }
 
 function convert(schema, html) {
