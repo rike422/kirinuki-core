638a1ece520a0226239954e20c83ad44f38ee843
Akira Nitta <rike422@gmail.com>
2018-08-22 14:43:19 +0900

    Add standalone build configuration

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import { uglify } from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/kirinuki.standalone.js',
+  output: {
+    file: 'lib/kirinuki.standalone.js',
+    name: 'kirinuki',
+    format: 'umd'
+  },
+  plugins: [
+    babel({
+      exclude: [
+        'node_modules/**',
+        '**/*.json'
+      ]
+    }),
+    resolve({
+      jsnext: true,
+      browser: true,
+      main: true
+    }),
+    json({}),
+    commonjs({
+      include: 'node_modules/**',
+      exclude: [
+        '**/*.json'
+      ]
+    }),
+    uglify()
+  ]
+};
